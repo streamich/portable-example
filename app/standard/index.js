30fe67b0e52edc66f213d2d2a8aea6bab702383d
streamich <team@streamich.com>
2015-06-19 03:24:10 +0100

    ...

--- app/standard/index.js
+++ app/standard/index.js
@@ -15,3 +15,13 @@ console.log("Now let's load additional features.");
 fs.mount('/my-app', 'https://127.0.0.1:1234/layers/feature.json', function() {
     require('../feature/print-time');
 });
+
+
+var Module = require('module');
+Module.async = function(args) {
+    args[1]('module ' + args[0]);
+};
+
+require('test', function(test) {
+    console.log(test);
+});
